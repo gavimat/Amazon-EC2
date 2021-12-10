1442f25675fef855298238604b025de67d66a993
Eduardo Mello Martins <78046473+EduardoMMartins@users.noreply.github.com>
2021-12-10 10:40:27 -0300

    Update createChannel.sh
    Alterações nos path

--- createChannel.sh
+++ createChannel.sh
@@ -48,7 +48,7 @@ createChannel(){
     rm -rf ./channel-artifacts/*
     setGlobalsForPeer0Org1
     
-    /root/fabric-samples/bin/peer channel create -o localhost:7050 -c $CHANNEL_NAME \
+    /home/ubuntu/fabric-samples/bin/peer channel create -o localhost:7050 -c $CHANNEL_NAME \
     --ordererTLSHostnameOverride orderer.example.com \
     -f ./artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
     --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
@@ -64,25 +64,25 @@ removeOldCrypto(){
 
 joinChannel(){
     setGlobalsForPeer0Org1
-    /root/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    /home/ubuntu/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
     setGlobalsForPeer1Org1
-    /root/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    /home/ubuntu/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
     setGlobalsForPeer0Org2
-    /root/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    /home/ubuntu/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
     setGlobalsForPeer1Org2
-    /root/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    /home/ubuntu/fabric-samples/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
 }
 
 updateAnchorPeers(){
     setGlobalsForPeer0Org1
-    /root/fabric-samples/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    /home/ubuntu/fabric-samples/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
     
     setGlobalsForPeer0Org2
-    /root/fabric-samples/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    /home/ubuntu/fabric-samples/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
     
 }
 echo "REMOVING OLD CRYPTO"
